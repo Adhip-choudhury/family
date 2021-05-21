6fe5e2bd2f1a2fd7ebb0ee78c35cb307e1de0de0
Adhip-choudhury <79558085+Adhip-choudhury@users.noreply.github.com>
2021-05-21 21:04:03 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+var member=
+["https://pngfile.net/public/uploads/preview/business-man-cartoon-png-free-download-31567109436y3oqiw5stw.png",]
+function Nextvv() {
+document.getElementsById("9404044xccc").innerHTML.member;
+}
